1863f51cb23d5ab2b69b3e2cf0714e35ed6a25aa
Neslisah Koc <neslisah.koc@hs-furtwangen.de>
2021-10-14 11:58:32 +0200

    variable mit _

--- A01_RandomPoem/RandomPoem.js
+++ A01_RandomPoem/RandomPoem.js
@@ -1,16 +1,16 @@
 "use strict";
 var RandomPoem;
 (function (RandomPoem) {
-    let _subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let _praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert", " zerstört"];
-    let _objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert", " zerstört"];
+    let objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     // console.log(subjekte, praedikate, objekte);
-    for (let i = _subjekte.length; i >= 1; i--) {
+    for (let i = subjekte.length; i >= 1; i--) {
         //console.log(i);
-        let werteAusgeben = getVerse(_subjekte, _praedikate, _objekte);
+        let werteAusgeben = getVerse(subjekte, praedikate, objekte);
         console.log(werteAusgeben);
     }
-    function getVerse(subjekte, praedikate, objekte) {
+    function getVerse(_subjekte, _praedikate, _objekte) {
         let wert1 = Math.floor(Math.random() * subjekte.length);
         let wert2 = Math.floor(Math.random() * praedikate.length);
         let wert3 = Math.floor(Math.random() * objekte.length);
--- A01_RandomPoem/RandomPoem.ts
+++ A01_RandomPoem/RandomPoem.ts
@@ -1,19 +1,19 @@
 namespace RandomPoem {
-    let _subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" ];
-    let _praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", " zerstört" ];
-    let _objekte: string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren" ];
+    let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" ];
+    let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", " zerstört" ];
+    let objekte: string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren" ];
    // console.log(subjekte, praedikate, objekte);
 
 
-    for (let i: number = _subjekte.length; i >= 1 ; i--) {
+    for (let i: number = subjekte.length; i >= 1 ; i--) {
       //console.log(i);
 
-      let werteAusgeben: string = getVerse(_subjekte, _praedikate, _objekte);
+      let werteAusgeben: string = getVerse(subjekte,  praedikate, objekte);
       console.log(werteAusgeben);
         
     }
 
-    function getVerse(subjekte: string[], praedikate: string[], objekte: string[]): string {
+    function getVerse(_subjekte: string[], _praedikate: string[], _objekte: string[]): string {
         let wert1: number = Math.floor(Math.random() * subjekte.length);
         let wert2: number = Math.floor(Math.random() * praedikate.length);
         let wert3: number = Math.floor(Math.random() * objekte.length);
